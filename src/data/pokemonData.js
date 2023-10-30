6f842bedbf9a707883d4091fad6866f527002079
JikanSui <jikansui16@gmail.com>
2023-10-30 16:15:11 -0300

    react-project

---
+++ src/data/pokemonData.js
@@ -0,0 +1,11 @@
+import squirtleImage from "../assets/squirtle.png";
+import charmanderImage from "../assets/charmander.png";
+import onixImage from "../assets/onix.png";
+import pikachuImage from "../assets/pikachu.png";
+
+export const pokemonData = [
+	{ name: "Squirtle", image: squirtleImage, type: "Water", color: "#3498db", moves: "water gun" }, // Azul
+	{ name: "Charmander", image: charmanderImage, type: "Fire", color: "#e9933e", moves: "ember" }, // Naranja
+	{ name: "Onix", image: onixImage, type: "Rock", color: "#95a5a6", moves: "rock throw" }, // Gris
+	{ name: "Pikachu", image: pikachuImage, type: "Electric", color: "#f1c40f", moves: "thunder shock" }, // Amarillo
+];
